c14259c6fd31d08416939f9bf9710652bb0a84c2
Jonathan Bangert <jonathan@bangert.dk>
2023-04-17 13:30:07 +0200

    view

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use Illuminate\Contracts\View\View;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -14,5 +15,5 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return 'Whatup';
+    return View::make('test');
 });
